a45c8004fc9d1884bc699bf680cc43ac8733b269
Robert Moore <romoore@cs.rutgers.edu>
2012-05-02 15:12:50 -0400

    WIP.
    + Adding WAR support to pom.
    + Playing with fancier objects for app.

--- src/main/java/org/grailrtls/MyResource.java
+++ src/main/java/org/grailrtls/MyResource.java
@@ -4,19 +4,19 @@ package org.grailrtls;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
-// The Java class will be hosted at the URI path "/myresource"
-@Path("/myresource")
+// The Java class will be hosted at the URI path "/my/html"
+@Path("/my/html")
 public class MyResource {
 
     // TODO: update the class to suit your needs
     
     // The Java method will process HTTP GET requests
     @GET 
-    // The Java method will produce content identified by the MIME Media
-    // type "text/plain"
-    @Produces("text/plain")
-    public String getIt() {
-        return "Got it!";
+    @Produces("text/html")
+    public RESTGreeting getIt(@QueryParam("name")String name) {
+    	return new RESTGreeting(name);
+        
     }
 }
